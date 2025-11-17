4e0c572660edda0075176692b2b40d98242a9053
Sebasram11andes <js.ramirezg123@uniandes.edu.co>
2025-11-16 19:08:16 -0500

    Proyecto_names

--- bankchurn-api/app/config.py
+++ bankchurn-api/app/config.py
@@ -26,7 +26,7 @@ class Settings(BaseSettings):
         "https://localhost:8000",  # type: ignore
     ]
 
-    PROJECT_NAME: str = "Banckchurn API"
+    PROJECT_NAME: str = "Banckchurn API Juan Sebastian Ramirez Garcia"
 
     class Config:
         case_sensitive = True
